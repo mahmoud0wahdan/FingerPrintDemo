5d18e1c365d18e047f2eb0cfc356d2671fd189c3
Mahmoud Wahdan <mihamed@ntgclarity.com>
2021-09-02 14:45:52 +0200

    fix threading issue

--- app/src/main/java/com/example/demofingerprint/MainActivity.java
+++ app/src/main/java/com/example/demofingerprint/MainActivity.java
@@ -7,6 +7,7 @@ import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,18 +36,18 @@ public class MainActivity extends AppCompatActivity implements IBScanListener, I
         m_ibScan.setScanListener(this);
         final UsbManager manager = (UsbManager) this.getApplicationContext().getSystemService(Context.USB_SERVICE);
         final HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
-        mobileLog.append("searching for connected device while looping");
+       logOnUIThread("searching for connected device while looping");
         if (deviceList.isEmpty()) {
-            mobileLog.append("\ndevice list is empty");
+           logOnUIThread("\ndevice list is empty");
         } else {
             for (UsbDevice device : deviceList.values()) {
                 final boolean isScanDevice = IBScan.isScanDevice(device);
-                mobileLog.append("\nfound a device but validate if it is a scan device");
+               logOnUIThread("\nfound a device but validate if it is a scan device");
                 if (isScanDevice) {
-                    mobileLog.append("\nfound an IbScan device and check for permission");
+                   logOnUIThread("\nfound an IbScan device and check for permission");
                     final boolean hasPermission = manager.hasPermission(device);
                     if (!hasPermission) {
-                        mobileLog.append("\nRequest an IbScan device permission");
+                       logOnUIThread("\nRequest an IbScan device permission");
                         this.m_ibScan.requestPermission(device.getDeviceId());
                     }
                 }
@@ -57,128 +58,126 @@ public class MainActivity extends AppCompatActivity implements IBScanListener, I
 
     @Override
     public void deviceCommunicationBroken(IBScanDevice ibScanDevice) {
-        mobileLog.append("\ndevice Communication Broken");
+       logOnUIThread("\ndevice Communication Broken");
     }
 
     @Override
     public void deviceImagePreviewAvailable(IBScanDevice ibScanDevice, IBScanDevice.ImageData imageData) {
-        mobileLog.append("\ndevice Image Preview Available");
+       logOnUIThread("\ndevice Image Preview Available");
     }
 
     @Override
     public void deviceFingerCountChanged(IBScanDevice ibScanDevice, IBScanDevice.FingerCountState fingerCountState) {
-        mobileLog.append("\ndevice Finger Count Changed");
+       logOnUIThread("\ndevice Finger Count Changed");
     }
 
     @Override
     public void deviceFingerQualityChanged(IBScanDevice ibScanDevice, IBScanDevice.FingerQualityState[] fingerQualityStates) {
-        mobileLog.append("\ndevice Finger Quality Changed");
+       logOnUIThread("\ndevice Finger Quality Changed");
     }
 
     @Override
     public void deviceAcquisitionBegun(IBScanDevice ibScanDevice, IBScanDevice.ImageType imageType) {
-        mobileLog.append("\ndevice Acquisition Begun");
+       logOnUIThread("\ndevice Acquisition Begun");
     }
 
     @Override
     public void deviceAcquisitionCompleted(IBScanDevice ibScanDevice, IBScanDevice.ImageType imageType) {
-        mobileLog.append("\ndevice Acquisition Completed");
+        logOnUIThread("\ndevice Acquisition Completed");
         m_ibScanDevice = ibScanDevice;
     }
 
     @Override
     public void deviceImageResultAvailable(IBScanDevice ibScanDevice, IBScanDevice.ImageData imageData, IBScanDevice.ImageType imageType, IBScanDevice.ImageData[] imageData1) {
-        mobileLog.append("\nIbScan device image now available with original size " + imageType);
+       logOnUIThread("\nIbScan device image now available with original size " + imageType);
     }
 
     @Override
     public void deviceImageResultExtendedAvailable(IBScanDevice ibScanDevice, IBScanException e, IBScanDevice.ImageData imageData, IBScanDevice.ImageType imageType, int i, IBScanDevice.ImageData[] imageData1, IBScanDevice.SegmentPosition[] segmentPositions) {
-        mobileLog.append("\nIbScan device image now available with Extended size " + imageType);
+       logOnUIThread("\nIbScan device image now available with Extended size " + imageType);
     }
 
     @Override
     public void devicePlatenStateChanged(IBScanDevice ibScanDevice, IBScanDevice.PlatenState platenState) {
-        mobileLog.append("\ndevice Platen State Changed");
+       logOnUIThread("\ndevice Platen State Changed");
     }
 
     @Override
     public void deviceWarningReceived(IBScanDevice ibScanDevice, IBScanException warning) {
-        mobileLog.append("\nIbScan device warning " + warning.getType().toString() + " " + warning.getMessage());
+       logOnUIThread("\nIbScan device warning " + warning.getType().toString() + " " + warning.getMessage());
     }
 
     @Override
     public void devicePressedKeyButtons(IBScanDevice ibScanDevice, int i) {
-        mobileLog.append("\ndevice Pressed Key Buttons");
+       logOnUIThread("\ndevice Pressed Key Buttons");
     }
 
     @Override
     public void scanDeviceAttached(int deviceId) {
-        mobileLog.append("\nIbScan device attached " + deviceId);
+       logOnUIThread("\nIbScan device attached " + deviceId);
 
         final boolean hasPermission = m_ibScan.hasPermission(deviceId);
         if (!hasPermission) {
             m_ibScan.requestPermission(deviceId);
         } else {
-            mobileLog.append("\nopening device at index 0");
-            runOnUiThread(() -> {
+           logOnUIThread("\nopening device at index 0");
+            new Thread(() -> {
                 try {
-                    mobileLog.append("\nIbScan device permission Request granted " + deviceId);
                     m_ibScanDevice = m_ibScan.openDevice(0);
                 } catch (IBScanException e) {
-                    e.printStackTrace();
+                   logOnUIThread(e.getType().name());
                 }
-            });
+            }).start();
         }
     }
 
     @Override
     public void scanDeviceDetached(int deviceId) {
-        mobileLog.append("\nIbScan device Detached " + deviceId);
+       logOnUIThread("\nIbScan device Detached " + deviceId);
     }
 
     @Override
     public void scanDevicePermissionGranted(int deviceId, boolean granted) {
         if (granted) {
-            runOnUiThread(() -> {
-                try {
-                    mobileLog.append("\nIbScan device permission Request granted " + deviceId);
-                    m_ibScanDevice = m_ibScan.openDevice(0);
-                } catch (IBScanException e) {
-                    e.printStackTrace();
-                }
-            });
+
+           logOnUIThread("\nIbScan device permission Request granted");
+
             // showToastOnUiThread("Permission granted to device " + deviceId, Toast.LENGTH_SHORT);
         } else {
-            mobileLog.append("\nIbScan device permission Request denied");
+           logOnUIThread("\nIbScan device permission Request denied");
             //  showToastOnUiThread("Permission denied to device " + deviceId, Toast.LENGTH_SHORT);
         }
     }
 
     @Override
     public void scanDeviceCountChanged(int deviceId) {
-        mobileLog.append("\nscan Device Count Changed " + deviceId);
+       logOnUIThread("\nscan Device Count Changed " + deviceId);
     }
 
     @Override
     public void scanDeviceInitProgress(int deviceIndex, int progressValue) {
-        mobileLog.append("\nscanDeviceInitProgress " + progressValue + " %");
+       logOnUIThread("\nscanDeviceInitProgress " + progressValue + " %");
     }
+
 
     @Override
     public void scanDeviceOpenComplete(int deviceIndex, IBScanDevice ibScanDevice, IBScanException e) {
-        if (ibScanDevice == null) {
-            mobileLog.append("\nCan't open the scan device because " + e.getMessage());
-        } else {
-            mobileLog.append("\nscan Device Open Complete " + deviceIndex);
-            m_ibScanDevice = ibScanDevice;
-            try {
-                ibScanDevice.beginCaptureImage(IBScanDevice.ImageType.FLAT_SINGLE_FINGER, IBScanDevice.ImageResolution.RESOLUTION_500, IBScanDevice.OPTION_AUTO_CAPTURE);
+       runOnUiThread(() -> {
+           if (ibScanDevice == null) {
+               logOnUIThread("\nCan't open the scan device because " + e.getMessage());
+           } else {
+               logOnUIThread("\nscan Device Open Complete " + deviceIndex);
+               m_ibScanDevice = ibScanDevice;
+               try {
+                   ibScanDevice.beginCaptureImage(IBScanDevice.ImageType.FLAT_SINGLE_FINGER, IBScanDevice.ImageResolution.RESOLUTION_500, IBScanDevice.OPTION_AUTO_CAPTURE);
 
-            } catch (IBScanException ibScanException) {
-                mobileLog.append("\nscan Device Open Complete " + e.getMessage());
-                //  Log.e(Constants.ERROR, "Error Capturing Image because " + e.getMessage());
-            }
-        }
+               } catch (IBScanException ibScanException) {
+                   logOnUIThread("\nscan Device Open Complete " + e.getType().name());
+                   //  Log.e(Constants.ERROR, "Error Capturing Image because " + e.getMessage());
+               }
+           }
+       });
+
     }
 
     @Override
@@ -219,4 +218,17 @@ public class MainActivity extends AppCompatActivity implements IBScanListener, I
         }
     }
 
+    public void openDevice(View view) {
+        logOnUIThread("open Device clicked");
+        new Thread(() -> {
+            try {
+                m_ibScanDevice = m_ibScan.openDevice(0);
+            } catch (IBScanException e) {
+               logOnUIThread(e.getType().name());
+            }
+        }).start();
+    }
+    private void logOnUIThread(String message){
+      runOnUiThread(() ->mobileLog.append("\n"+message));
+    }
 }
